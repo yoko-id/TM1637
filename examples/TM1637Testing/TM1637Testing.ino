c1345f53cfc3ca619f0d8b87cc4ae776dcb807b9
yoko <yoko-id@users.noreply.github.com>
2021-04-24 20:22:02 +0700

    Update TM1637Testing.ino

--- examples/TM1637Testing/TM1637Testing.ino
+++ examples/TM1637Testing/TM1637Testing.ino
@@ -36,52 +36,69 @@ void setup()
   //display.begin();
   //display.setBrightness(5, true);
   //display.setBrightness(2, true); // Turn on
+  display.setBrightness(0x01);
 }
 
 void loop()
 {
-  int k;
-  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-  uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
-  display.setBrightness(0x01);
+  /*int k;
+    uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+    uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
 
-  // All segments on
-  display.setSegments(data);
-  delay(TEST_DELAY);
-
-  // Selectively set different digits
-  data[0] = display.encodeDigit(0);
-  data[1] = display.encodeDigit(1);
-  data[2] = display.encodeDigit(2);
-  data[3] = display.encodeDigit(3);
-  display.setSegments(data);
-  delay(TEST_DELAY);
-  //Isi CALL
-  data[0] = display.encodeDigit(12); // C
-  data[1] = display.encodeDigit(10); // A
-  data[2] = display.encodeDigit(21); // L
-  data[3] = display.encodeDigit(21); // L
-  display.setSegments(data);
-  delay(TEST_DELAY);
-  /* //Isi CALL
-    display.setSegments(display.encodeDigit(27)); //r
-    display.setSegments(display.encodeDigit(14)); //E
-    display.setSegments(display.encodeDigit(12)); //C
-    display.setSegments(display.encodeDigit(21)); //L
-    delay(TEST_DELAY);
-    //Isi Entr
-    display.setSegments(display.encodeDigit(14)); //E
-    display.setSegments(display.encodeDigit(23)); //n
-    display.setSegments(display.encodeDigit(29)); //t
-    display.setSegments(display.encodeDigit(27)); //r
-    delay(TEST_DELAY);
-  */
+    // All segments on
+    display.setSegments(blank);
+    delay(TEST_DELAY);*/
   /*
-    for(k = 3; k >= 0; k--) {
-    display.setSegments(data, 1, k);
-    delay(TEST_DELAY);
-    }
+      // Selectively set different digits
+      data[0] = display.encodeDigit(0);
+      data[1] = display.encodeDigit(1);
+      data[2] = display.encodeDigit(2);
+      data[3] = display.encodeDigit(3);
+      display.setSegments(data);
+      delay(TEST_DELAY);
+      //Isi CALL
+      data[0] = display.encodeDigit(12); // C
+      data[1] = display.encodeDigit(10); // A
+      data[2] = display.encodeDigit(21); // L
+      data[3] = display.encodeDigit(21); // L
+      display.setSegments(data);
+      delay(TEST_DELAY);
+      //Isi rECL
+      data[0] = display.encodeDigit(27); // r
+      data[1] = display.encodeDigit(14); // E
+      data[2] = display.encodeDigit(12); // C
+      data[3] = display.encodeDigit(21); // L
+      display.setSegments(data);
+      delay(TEST_DELAY);
+      //Isi Entr
+      data[0] = display.encodeDigit(14); // E
+      data[1] = display.encodeDigit(23); // n
+      data[2] = display.encodeDigit(29); // t
+      data[3] = display.encodeDigit(27); // r
+      display.setSegments(data);
+      delay(TEST_DELAY);
+      data[0] = display.encodeDigit(10); // A
+      data[1] = display.encodeDigit(0);  // 0
+      data[2] = display.encodeDigit(0);  // 0
+      data[3] = display.encodeDigit(7);  // 7
+      display.setSegments(data);
+      delay(TEST_DELAY);
   */
+  //data[0] = display.encodeDigit(10); // A
+  // display.setSegments(1, 3, 0);
+  //display.show(1, 2, 3, 4);
+  //delay(TEST_DELAY);
+  for (byte k = 0; k < 38; k++) {
+    //display.show(k, k - 1, k - 2, k - 3);
+    display.show(k, k, k, k );
+    //display.show(k, 1);
+    delay(750);
+  }
+  /* for (k = 3; k >= 0; k--) {
+     display.setSegments(data, 1, k);
+     delay(TEST_DELAY);
+    }*/
+
   /*
     display.clear();
     display.setSegments(data + 2, 2, 2);
